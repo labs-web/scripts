0174f0ee68a9ac920bf94aa0bdd7ab415ed34f6f
ESSARRAJ <essarraj.fouad@gmail.com>
2024-01-22 20:59:58 +0100

    save

--- issues/create-or-update.ps1
+++ issues/create-or-update.ps1
@@ -1,118 +1,168 @@
-﻿# Création des issue 
+﻿# Create or updat backlog to issues
+
+# Le sctipy doit être exécuter dans la racine de dépôt
+
+# 
+# Description
+# 
 # - Création des issues 
 # - Mise à jour des issues 
 # - Affectation de l'issue à TeamPlanning
+# - Nom de fichier : 1.nom_issue.23.md
 
-# Le sctipy doit être exécuter dans le dossier racine de dépôt
-
-# Organisation name
-function get_org_name {
-    $current_script_path = $PSScriptRoot.Split('\')
-    return $current_script_path[1] 
-}
-$project_name = get_org_name
-
-# Encoding utf8
-$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 $prev = [Console]::OutputEncoding
 [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
 
 
+#
+# Functions : Message de confirmation
+#
 
-# Liste des éléments de backlog 
-$depot_path = Get-Location
-
-
-# Function : Message de confirmation
 function confirm_to_continue($message) {
-    $title    = $message 
-    $question = "Are you sure you want to proceed?"
-    $choices  = '&Yes', '&No'
-    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-    if ($decision -eq 1) {
-        exit
-    } 
+      
+  $title    = $message 
+  $question = "Are you sure you want to proceed?"
+  $choices  = '&Yes', '&No'
+
+  # Write-Host $message 
+  
+  # $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+  # if ($decision -eq 1) {
+  #  exit
+  # } 
+}
+# get organisation name
+function get_org_name {
+  $current_script_path = $PSScriptRoot.Split('\')
+  return $current_script_path[1] 
 }
 
 
 
 function find_issue_by_title($title){
-    $all_issues = gh issue list --json number,title | ConvertFrom-Json
-    
-    foreach($issue in  $all_issues){
-        # Write-Host $issue.title
-        if($issue.title -eq $title){
-            return $issue
-        }
-    }
-    return $null
-}
-# $issue_test = find_issue_by_title("backlog")
-# Write-Host( $issue_test)
-# $issue_test = find_issue_by_title("suivi-réalisation-labs-web")
-# Write-Host( $issue_test)
+  
+  # confirm_to_continue("find $title in issues ")
+  
+  $all_issues = gh issue list --json number,title | ConvertFrom-Json
 
+  foreach($issue in  $all_issues){
+    # Write-Host $Issue_obj.title
+    if($issue.title -eq $title){
+      return $issue
+    }
+  }
+  return $null
+}
+
+function get_issue_object([String]$file_name, [String] $file_fullname){
+  $item_full_path = Split-Path  -Path $file_fullname
+  # Règle : L'issue est existe si le fichier item commence par le numéro de l'issue
+  # Exemple des nom 
+  # issue: 2.conception.10.md : 2:ordre,conception:title,10:numéro de l'issue sur github
+  # order_file:   3.codage.md
+  # create_file : test.md
+  # State : create_file,order_file,issue
+  $Issue_obj = [PSCustomObject]@{ordre = 0
+    number =  0
+    title = ''
+    labels = ''
+    state = ''
+    member = $null
+  }
+  $file_name_array = $file_name.Split(".")
+  # si file is issue : l'élément avant dernier est un nombre
+  $last_element_index = $file_name_array.Length - 1 
+  $avant_dernier_element = $file_name_array[$last_element_index - 1]
+  $first_element = $file_name_array[0]
+
+
+  # Titre : si l'avant dernier élémet est un nombre 
+  if($avant_dernier_element -match "^\d+$")
+  {
+    $Issue_obj.number = $avant_dernier_element
+    $Issue_obj.title = $file_name_array[$last_element_index - 2]
+  } else
+  {
+    $Issue_obj.title = $file_name_array[$last_element_index - 1]
+    $Issue_obj.number = 0
+  }
+
+
+  # si number = 0 et issue existe dans github
+  $issue = find_issue_by_title $Issue_obj.title
+  if($Issue_obj.number -eq 0){
+    if(-not($issue -eq $null)){
+      $Issue_obj.number = $issue.number
+    }
+  }else{
+    if($issue -eq $null){
+      $Issue_obj.number = "0"
+      Write-Host("Issue avec le nombre $($Issue_obj.number) doit être existe sur github")
+      exit 1
+    }
+  }
+    
+  # Dection de membre 
+  $membre_title_array = $Issue_obj.title.Split("_")
+  if($membre_title_array.Length -eq 2){
+    $Issue_obj.member = $membre_title_array[0]
+  }else{ 
+    $Issue_obj.member = $null
+  }
+   
+    
+  # L'odre est le premier nombre
+  if($first_element -match "^\d+$") { 
+    $Issue_obj.ordre = $first_element
+  } 
+  else{ 
+    $Issue_obj.ordre = "0"
+  }
+  
+  
+  return $Issue_obj
+}
+
+# Input
+$depot_path = Get-Location
+$project_name = get_org_name
+
+# Message de confirmation
 confirm_to_continue("Update or Create issues for repository : $depot_path ")
 
+
+# Traitement pour chaque fichier(item) dans /backlog
 Get-ChildItem "$depot_path/backlog" -Filter *.md | 
 Foreach-Object {
 
-    # item : name,path
-    $item_full_name = $_.FullName
-    $item_name = $_.Name
-    $item_full_path = Split-Path  -Path $item_full_name
+    # file name and path
+    $file_fullname = $_.FullName
+    $file_name = $_.Name
+    $item_full_path = Split-Path  -Path $file_fullname
+    # issue_object
+    $Issue_obj = get_issue_object $file_name  $file_fullname
 
- 
-    # L'issue est existe si le fichier item commence par le numéro de l'issue
-    $item_name_array = $item_name.Split(".");
-    $issue_titre = ""
-    $issue_number = ""
-
-    # Create or Update
-    # item = issue => number.titre.md
-    if( $item_name_array.Length -eq 3 ) { 
-        $issue_titre = $item_name_array[1]
-        $issue_number = $item_name_array[0]
-        Write-Host("Update de l'issue $issue_titre ")
-        # confirm_to_continue("Update de l'issue $issue_titre ")
-        gh issue edit $issue_number --title $issue_titre --add-label feature,new_issue --add-project $project_name --body-file $item_full_name
+    # Create new issue 
+    if($Issue_obj.number -eq 0){
+            confirm_to_continue("Création de l'issue : $Issue_obj ")
+            if($Issue_obj.member -eq $null){
+                gh issue create --title $Issue_obj.title--label feature,new_issue --project $project_name  --body-file $file_fullname
+            }else{
+                gh issue create --title $Issue_obj.title --label feature,new_issue --assignee $Issue_obj.member  --project $project_name  --body-file $file_fullname 
+            }
     }else{
-
-        # Create issue
-        $issue_titre = $item_name_array[0]
-
-        # Dection de membre 
-        $membre_titre_array = $issue_titre.Split("_");
-        Write-Host("membre_titre_array = $membre_titre_array")
-        $membre_reference = $null
-        if($membre_titre_array.Length -eq 2){
-            $membre_reference = $membre_titre_array[0]
-        }
-
-               
-
-        Write-Host("Création de l'issue : $issue_titre ")
-        # confirm_to_continue("Création de l'issue : $issue_titre pour $membre_reference ")
-
-        if($membre_reference -eq $null){
-            gh issue create --title $issue_titre --label feature,new_issue --project $project_name  --body-file $item_full_name
-        }else{
-            gh issue create --title $issue_titre --label feature,new_issue --assignee $membre_reference  --project $project_name  --body-file $item_full_name 
-        }
-
-
-        # Update item file name
-        $created_issue = find_issue_by_title($issue_titre)
-        $issue_number = $created_issue.number
-        if($created_issue -eq $null){
-            Write-Host "L'issue $item_full_name doit être existe dans le dépôt aprés sa création"
-            exit
-        }else{
-            Write-Host "Rename item file name : $issue_titre"
-            # Change issue file name 
-            $item_new_name = "$issue_number.$issue_titre.md"
-            Rename-Item -Path $item_full_name -NewName "$item_full_path\$item_new_name"
-        }
+        # Edit existant issue
+        confirm_to_continue("Update de l'issue $($Issue_obj) ")
+        gh issue edit $Issue_obj.number --title $Issue_obj.title --add-label feature,new_issue --add-project $project_name --body-file $file_fullname
     }
+
+    # Change file name if is incorrect
+    $Issue_obj_file_name = "$($Issue_obj.ordre).$($Issue_obj.title).$($Issue_obj.number).md"
+    if(-not($Issue_obj_file_name -eq $file_name )){
+        # Update file name
+        Write-Host "Rename $file_name to $Issue_obj_file_name "
+        Rename-Item -Path $file_fullname -NewName "$item_full_path\$Issue_obj_file_name"
+    }
+
 }
--- labs/update-readme-file.ps1
+++ labs/update-readme-file.ps1
@@ -1,6 +1,11 @@
-﻿# Update README file for lab
+﻿# Update README.md file from lab-web.json
+
+
+# Le code source est dévelopé dans /labs-web/scripts/labs/update-readme-file.ps1
  
-# Functions : confirmation
+# L'utilisateur doit exécuter ce script sur la racine du lab
+
+# Functions
 function confirm_to_continue($message) {
     $title    = $message 
     $question = "Are you sure you want to proceed?"
@@ -11,35 +16,47 @@ function confirm_to_continue($message) {
     } 
 }
  
-# L'utilisateur doit exécuter ce script sur le racine du lab
-# confirm_to_continue("Vous devez exécuter ce script sur la racine du lab")
-
-# Paths 
+# Input 
 $depot_path = $(Get-Location).Path
 $readme_path= "$depot_path\README.md"
-$lab_web_data = "$depot_path/lab-web.json"
-$backlog_files_path = "$depot_path\Backlog"
-# Confirmation
-# confirm_to_continue("Update de fichier $readme_path ")
+$lab_web_data_path = "$depot_path/lab-web.json"
+$backlog_files_path = "$depot_path\backlog"
 
+# Load json_data
+$json_data = Get-Content $lab_web_data_path  | ConvertFrom-Json
 
-# Load JSON
-$json_data = Get-Content $lab_web_data  | ConvertFrom-Json
+# 
+# Génération de contenue de fichier README
+#
 
-
-# Création de fichier README
+# Introduction
 $readme_string = "# $($json_data.Introduction.Titre) `n`n"
-$readme_string += "- Référence :  $($json_data.Introduction.Référence) `n`n"
+$readme_string += "- Référence :  $($json_data.Introduction.Reference) `n`n"
 $readme_string += "$($json_data.Introduction.Description) `n`n"
-
 # Backlog
 $readme_string += "## $($json_data.Backlog.Titre) `n`n"
 $readme_string += "$($json_data.Backlog.Introduction) `n`n"
-
 Get-ChildItem $backlog_files_path |  ForEach-Object {
     $backlog_item_file_name = $_.Name
     $readme_string += "- [$backlog_item_file_name](./Backlog/$backlog_item_file_name) `n"
 } 
+## Livrables 
+$readme_string += "## $($json_data.Livrables.Titre) `n`n"
+$readme_string += "$($json_data.Livrables.Introduction) `n`n"
+$json_data.Livrables.Livrables |  ForEach-Object {
+    $readme_string += "- $($_.Titre) `n"
+    if(-not($_.Description -eq "")){
+        $readme_string += "  - $($_.Description) `n"
+    }
+} 
+## Références 
+$readme_string += "## $($json_data.References.Titre) `n`n"
+$readme_string += "$($json_data.References.Introduction) `n`n"
+
+$json_data.References.References |  ForEach-Object {
+    $readme_string += "- [$($_.Titre)]($($_.Lien)) `n"
+} 
+
 
 # Enregistrement de fichier README
 Set-Content $readme_path $readme_string
