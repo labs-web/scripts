85135304b445f9210cba0e16201c6b338c7c65e9
ESSARRAJ Fouad <fouad.systems@gmail.com>
2024-01-22 12:56:58 +0100

    Update README file for lab

--- labs/update-readme-file.ps1
+++ labs/update-readme-file.ps1
@@ -1,10 +1,5 @@
 ﻿# Update README file for lab
  
-# Encoding utf8
-$PSDefaultParameterValues['*:Encoding'] = 'utf8'
-$prev = [Console]::OutputEncoding
-[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
-
 # Functions : confirmation
 function confirm_to_continue($message) {
     $title    = $message 
@@ -17,14 +12,13 @@ function confirm_to_continue($message) {
 }
  
 # L'utilisateur doit exécuter ce script sur le racine du lab
-confirm_to_continue("Vous devez exécuter ce script sur la racine du lab")
+# confirm_to_continue("Vous devez exécuter ce script sur la racine du lab")
 
-# lab-reference
-
-$depot_path = Get-Location
-$depot_path = $depot_path.Path
+# Paths 
+$depot_path = $(Get-Location).Path
 $readme_path= "$depot_path\README.md"
 $lab_web_data = "$depot_path/lab-web.json"
+$backlog_files_path = "$depot_path\Backlog"
 # Confirmation
 # confirm_to_continue("Update de fichier $readme_path ")
 
@@ -34,9 +28,18 @@ $json_data = Get-Content $lab_web_data  | ConvertFrom-Json
 
 
 # Création de fichier README
-$readme_string = "# $($json_data.Introduction.Titre) `n"
+$readme_string = "# $($json_data.Introduction.Titre) `n`n"
 $readme_string += "- Référence :  $($json_data.Introduction.Référence) `n`n"
-$readme_string += "$($json_data.Introduction.Description) `n"
+$readme_string += "$($json_data.Introduction.Description) `n`n"
+
+# Backlog
+$readme_string += "## $($json_data.Backlog.Titre) `n`n"
+$readme_string += "$($json_data.Backlog.Introduction) `n`n"
+
+Get-ChildItem $backlog_files_path |  ForEach-Object {
+    $backlog_item_file_name = $_.Name
+    $readme_string += "- [$backlog_item_file_name](./Backlog/$backlog_item_file_name) `n"
+} 
 
 # Enregistrement de fichier README
 Set-Content $readme_path $readme_string
