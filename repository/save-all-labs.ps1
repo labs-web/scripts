53ffe5197c5c3bc24ac8089b1da4d9ac0268d026
ESSARRAJ <essarraj.fouad@gmail.com>
2024-01-26 18:10:05 +0100

    save

--- repository/save-all-labs.ps1
+++ repository/save-all-labs.ps1
@@ -1,7 +1,18 @@
 ﻿# Enregistrement de tous les labs 
+Write-Host "`n Ce script ne peut pas être exécuter dans github action `n"
+
+
+. "./scripts/core/core.ps1"
+# Core : Params
+$debug = $true
+$confirm_message = $false
+
 
 # Emplacement des labs 
-$labs_web_path = $PSScriptRoot + "/../../"
+$depot_path = $(Get-Location).Path
+$labs_web_path = $depot_path+ "/../"
+
+confirm_to_continue "Push $labs_web_path"
 cd $labs_web_path
 
 Get-ChildItem . -Filter * | 
@@ -15,6 +26,7 @@ Foreach-Object {
     Write-Host $FullName
 
     # git push
+    confirm_to_continue "push $FullName"
     cd $FullName
     git add .
     git commit -m "save"
