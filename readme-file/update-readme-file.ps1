83f65f1991adba5a7be8f718c814123466579c24
ESSARRAJ <essarraj.fouad@gmail.com>
2024-01-26 13:41:24 +0100

    save

--- readme-file/update-readme-file.ps1
+++ readme-file/update-readme-file.ps1
@@ -1,59 +1,47 @@
 ﻿# Update README.md file from lab-web.json
-
-
-# Le code source est dévelopé dans /labs-web/scripts/labs/update-readme-file.ps1
- 
 # L'utilisateur doit exécuter ce script sur la racine du lab
 
-# Functions
-function confirm_to_continue($message) {
-    $title    = $message 
-    $question = "Are you sure you want to proceed?"
-    $choices  = '&Yes', '&No'
-    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-    if ($decision -eq 1) { 
-        exit
-    } 
-}
- 
+. "./scripts/core/core.ps1"
+. "./scripts/core/pullrequest-core.ps1"
+
 # Input 
 $depot_path = $(Get-Location).Path
 $readme_path= "$depot_path\README.md"
-$lab_web_data_path = "$depot_path/lab-web.json"
+$readme_json_path = "$depot_path/README.json"
 $backlog_files_path = "$depot_path\backlog"
 
-# Load json_data
-$json_data = Get-Content $lab_web_data_path  | ConvertFrom-Json
+# Load readme_data
+$readme_data = Get-Content $readme_json_path  | ConvertFrom-Json
 
 # 
 # Génération de contenue de fichier README
 #
 
 # Introduction
-$readme_string = "# $($json_data.Introduction.Titre) `n`n"
-$readme_string += "- Référence :  $($json_data.Introduction.Reference) `n`n"
-$readme_string += "$($json_data.Introduction.Description) `n`n"
+$readme_string = "# $($readme_data.Introduction.Titre) `n`n"
+$readme_string += "- Référence :  $($readme_data.Introduction.Reference) `n`n"
+$readme_string += "$($readme_data.Introduction.Description) `n`n"
 # Backlog
-$readme_string += "## $($json_data.Backlog.Titre) `n`n"
-$readme_string += "$($json_data.Backlog.Introduction) `n`n"
+$readme_string += "## $($readme_data.Backlog.Titre) `n`n"
+$readme_string += "$($readme_data.Backlog.Introduction) `n`n"
 Get-ChildItem $backlog_files_path |  ForEach-Object {
     $backlog_item_file_name = $_.Name
     $readme_string += "- [$backlog_item_file_name](./Backlog/$backlog_item_file_name) `n"
 } 
 ## Livrables 
-$readme_string += "## $($json_data.Livrables.Titre) `n`n"
-$readme_string += "$($json_data.Livrables.Introduction) `n`n"
-$json_data.Livrables.Livrables |  ForEach-Object {
+$readme_string += "## $($readme_data.Livrables.Titre) `n`n"
+$readme_string += "$($readme_data.Livrables.Introduction) `n`n"
+$readme_data.Livrables.Livrables |  ForEach-Object {
     $readme_string += "- $($_.Titre) `n"
     if(-not($_.Description -eq "")){
         $readme_string += "  - $($_.Description) `n"
     }
 } 
 ## Références 
-$readme_string += "## $($json_data.References.Titre) `n`n"
-$readme_string += "$($json_data.References.Introduction) `n`n"
+$readme_string += "## $($readme_data.References.Titre) `n`n"
+$readme_string += "$($readme_data.References.Introduction) `n`n"
 
-$json_data.References.References |  ForEach-Object {
+$readme_data.References.References |  ForEach-Object {
     $readme_string += "- [$($_.Titre)]($($_.Lien)) `n"
 } 
 
