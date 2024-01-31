f76eea074d44c3afe348980c9d728718f4f72aaa
ESSARRAJ <essarraj.fouad@gmail.com>
2024-01-31 09:12:48 +0100

    add_task backlog

---
+++ tasks/start_task.ps1
@@ -0,0 +1,12 @@
+# Start Task 
+
+# Création d'un script qui permet de lancer le développement d'une tâche 
+
+# avec les étapes suivants :
+
+# - Création d'une branche
+# - Laison de la branche avec l'issue
+# - Création de pullrequest
+# - add label :
+#   - en_cours
+
