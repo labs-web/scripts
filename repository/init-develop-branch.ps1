af6b2778e0b3cbf865978fbf2350075409cd6868
ESSARRAJ Fouad <essarraj.fouad@gmail.Com>
2024-02-01 05:47:37 +0100

    save

---
+++ repository/init-develop-branch.ps1
@@ -0,0 +1,11 @@
+﻿# TODO : Script d'initialisation d'un lab
+
+# create develop branch
+git add .
+git commit -m "save"
+git push
+git checkout -b "develop"
+git push --set-upstream origin develop
+
+#  set develop branch as default 
+gh repo edit --default-branch develop
