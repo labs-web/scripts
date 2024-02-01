b6fae81f1e94b9f61be184c03fd9db0210148372
ESSARRAJ Fouad <essarraj.fouad@gmail.Com>
2024-02-01 06:33:30 +0100

    save

---
+++ repository/init-develop-all-repository.ps1
@@ -0,0 +1,42 @@
+﻿# Enregistrement de tous les labs 
+Write-Host "`n Ce script ne peut pas être exécuter dans github action `n"
+
+
+. "./scripts/core/core.ps1"
+# Core : Params
+$debug = $true
+$confirm_message = $false
+
+
+# Emplacement des labs 
+$depot_path = $(Get-Location).Path
+$labs_web_path = $depot_path+ "/../"
+
+confirm_to_continue "Push $labs_web_path"
+cd $labs_web_path
+
+Get-ChildItem . -Filter * | 
+Foreach-Object {
+
+    # Nom de lab
+    $FullName = $_.FullName
+    $File_Name = $_.Name
+
+    # Message de confirmation
+    Write-Host $FullName
+
+    # git push
+    confirm_to_continue "push $FullName"
+    cd $FullName
+
+    # create develop branch
+    git add .
+    git commit -m "save"
+    git push
+    git checkout -b "develop"
+    git push --set-upstream origin develop
+
+    #  set develop branch as default 
+    gh repo edit --default-branch develop
+
+}
