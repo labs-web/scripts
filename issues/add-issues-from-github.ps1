c79ac491137e1198d3e3669b35aca7c2d4be36d3
ESSARRAJ <essarraj.fouad@gmail.com>
2024-02-11 13:24:40 +0100

    fix add issues from github

---
+++ issues/add-issues-from-github.ps1
@@ -0,0 +1,96 @@
+﻿# Create or updat backlog to issues
+
+. "./scripts/core/core.ps1"
+. "./scripts/core/issue.core.ps1"
+. "./scripts/core/backlog.core.ps1"
+
+
+# Core : Params
+$debug = $true
+$test = $false
+$confirm_message = $false
+
+$depot_path = Get-Location
+
+
+function find_local_issue($remote_issue){
+
+  confirm_to_continue "find_local_issue $($remote_issue.title)"
+  
+  # Dans le dossier backlog, il existe plusieurs dossiers qui représente les labels 
+  $backlog_directories=  Get-ChildItem "$depot_path/backlog"  -Directory
+
+  foreach($backlog_directory in $backlog_directories) {
+
+      # Ne pas traiter les dossier qui commance par "_"
+      if($backlog_directory.Name -like "_*") {continue}
+
+      $label = $backlog_directory.Name
+      $directory = $backlog_directory.FullName 
+      
+      $backlog_items=  Get-ChildItem $directory -Filter *.md  
+      foreach($backlog_item in $backlog_items) {
+        # file name and path
+        $file_fullname = $backlog_item.FullName
+        $file_name = $backlog_item.Name
+        $item_full_path = Split-Path  -Path $file_fullname
+        # CreateIssue_obj that represente backlog_itm_file
+        $Issue_obj = get_issue_object $file_name  $file_fullname
+        if($Issue_obj.title -eq $remote_issue.title ){ 
+          return $true
+        }
+      }
+  }
+
+  return $false
+}
+
+
+# create backlog item if remote issue not exist in local backlog
+function create_remote_issue_in_backlog($item_full_path, $remote_issue ){
+
+  confirm_to_continue "create_remote_issue_in_backlog $remote_issue "
+
+  # find label 
+  $label = "feature" # défault label
+  foreach($remote_label in $remote_issue.labels){
+    if($remote_label.name -eq "feature") { $label = "feature"  }
+    if($remote_label.name -eq "exposé") { $label = "exposé"  }
+    if($remote_label.name -eq "chapitre") { $label = "chapitre"  }
+    if($remote_label.name -eq "thème") { $label = "thème"  }
+  }
+
+
+  # debug "Rename file : $Issue_obj"
+  $issue_file_name = "0.$($remote_issue.title).$($remote_issue.number).md"
+  
+  
+  
+  
+  $issue_file_full_name = "$item_full_path\$label\$issue_file_name"
+
+  # Update file name
+  debug "Create $issue_file_full_name"
+
+  new-item $issue_file_full_name
+  Set-Content $issue_file_full_name $remote_issue.body
+
+}
+
+
+
+# 
+# Algorithme 
+# 
+
+confirm_to_continue "add issue for $depot_path/backlog "
+
+
+$all_remote_issues = gh issue list -s all --json number,title,labels,body | ConvertFrom-Json
+foreach($remote_issue in  $all_remote_issues){
+  $local_issue_exist = find_local_issue $remote_issue
+  if($local_issue_exist -eq $false){
+    $item_full_path = "$depot_path/backlog"
+    create_remote_issue_in_backlog $item_full_path $remote_issue 
+  }
+}
--- issues/update-issues-from-backlog.ps1
+++ issues/update-issues-from-backlog.ps1
@@ -5,6 +5,8 @@
 . "./scripts/core/core.ps1"
 . "./scripts/core/issue.core.ps1"
 . "./scripts/core/pullrequest-core.ps1"
+. "./scripts/core/backlog.core.ps1"
+
 
 # Core : Params
 $debug = $true
@@ -34,61 +36,6 @@ confirm_to_continue("Update or Create issues for repository : $depot_path ")
 # Déclaration des fonctions 
 # 
 
-# Issue_obj : convert backlog_item_file to issue_obj
-function get_issue_object([String]$file_name, [String] $file_fullname){
-  # $item_full_path = Split-Path  -Path $file_fullname
-  # Règle : L'issue est existe si le fichier item commence par le numéro de l'issue
-  # Exemple des nom 
-  # issue: 2.conception.10.md : 2:ordre,conception:title,10:numéro de l'issue sur github
-  # order_file:   3.codage.md
-  # create_file : test.md
-  # State : create_file,order_file,issue
-  $Issue_obj = [PSCustomObject]@{ordre = 0
-    number =  0
-    title = ''
-    labels = ''
-    state = ''
-    body_file = $file_fullname
-    file_name = $file_name
-    member = $null
-  }
-  $file_name_array = $file_name.Split(".")
-  # si file is issue : l'élément avant dernier est un nombre
-  $last_element_index = $file_name_array.Length - 1 
-  $avant_dernier_element = $file_name_array[$last_element_index - 1]
-  $first_element = $file_name_array[0]
-  # Titre : si l'avant dernier élémet est un nombre 
-  if($avant_dernier_element -match "^\d+$")
-  {
-    $Issue_obj.number = $avant_dernier_element
-    $Issue_obj.title = $file_name_array[$last_element_index - 2]
-  } else
-  {
-    $Issue_obj.title = $file_name_array[$last_element_index - 1]
-    $Issue_obj.number = 0
-  }
-  # si number = 0 et issue existe dans github
-  $issue = find_issue_by_title $Issue_obj.title
-  if($Issue_obj.number -eq 0){
-    if(-not($issue -eq $null)){
-      $Issue_obj.number = $issue.number
-    }
-  }
-  # Detection de membre 
-  $membre_title_array = $Issue_obj.title.Split("_")
-  if($membre_title_array.Length -eq 2){
-    $Issue_obj.member = $membre_title_array[0]
-  }else{ 
-    $Issue_obj.member = $null
-  }
-  # L'odre est le premier nombre
-  if($first_element -match "^\d+$") { 
-    $Issue_obj.ordre = $first_element
-  } else{ 
-    $Issue_obj.ordre = "0"
-  }
-  return $Issue_obj
-}
 
 # Create new issue from $Issue_obj 
 function create_issue($Issue_obj,$label){
@@ -116,19 +63,6 @@ function edit_issue($Issue_obj,$label){
   gh issue edit $Issue_obj.number --title $Issue_obj.title --add-label $label --add-project $project_name --body-file $($Issue_obj.body_file)
 }
 
-function change_backlog_item_file_name($Issue_obj){
-  # debug "Rename file : $Issue_obj"
-  $Issue_obj_file_name = "$($Issue_obj.ordre).$($Issue_obj.title).$($Issue_obj.number).md"
-    if(-not($Issue_obj_file_name -eq $Issue_obj.file_name )){
-        # Update file name
-        debug "Rename $file_name to $Issue_obj_file_name "
-        debug "- Source : $file_fullname"
-        debug "- Destination : $item_full_path\$Issue_obj_file_name"
-        Rename-Item -Path $file_fullname -NewName "$item_full_path/$Issue_obj_file_name"
-        return $true
-    }
-    return $false
-}
 
 $add_or_update_issues_iteration = 0
 
@@ -139,7 +73,7 @@ function add_or_update_issues($directory, $label){
 
   $backlog_items=  Get-ChildItem $directory -Filter *.md  
   $add_or_update_issues_chaned_files = $false
-  
+
   foreach($backlog_item in $backlog_items) {
     # file name and path
     $file_fullname = $backlog_item.FullName
@@ -151,7 +85,7 @@ function add_or_update_issues($directory, $label){
     }else{ edit_issue $Issue_obj $label }
 
     # Change backlog_item_file name
-    $change_backlog_item_file_name_retuen_value = change_backlog_item_file_name $Issue_obj
+    $change_backlog_item_file_name_retuen_value = change_backlog_item_file_name $file_fullname $file_name $Issue_obj
 
      # if not yet true
     if($change_backlog_item_file_name_retuen_value) {
