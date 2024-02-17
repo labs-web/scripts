32abee877cb43c25356c0b970cbbcd8e7466b044
ESSARRAJ <essarraj.fouad@gmail.com>
2024-02-17 09:58:45 +0100

    save

--- code-review/code-review.ps1
+++ code-review/code-review.ps1
@@ -13,29 +13,29 @@ $autorized_directories = "app/Http/Controllers/$package_name",
                  "app/Models/$package_name",
                  "docs/$package_name"
 
+$autorised_change = $true
 
 foreach($file in $chanded_files){
 
-    $autorised_change = $false
+    $autorised_change_file = $false
 
     # Vérifier si le fichier $file est situé dans l'un des dossiers autorisé
     # TODO : utilisez des expression régulière
     foreach($autorized_directory in $autorized_directories ){
         if($file -like "$autorized_directory*"){
-            $autorised_change = $true
+            $autorised_change_file = $true
         }
     }
 
     # Afficahge de message d'erreur sir le membre n'est pas autorisé à modifier le fichier
-    if(-not($autorised_change)) {
+    if(-not($autorised_change_file)) {
         Write-Host "::error:: Vous n'avez pas le droit de modifier le fichier : $file"
+        $autorised_change = $false
     } 
 }
 
-# if(-not($message_erreur -eq "")){
-#     # $message_erreur = "::error  "  + $message_erreur
-#     # Write-Host $message_erreur
-#     exit 1
-# }
+if(-not($autorised_change)){
+    exit 1
+}
 
 
