28860073dae3ec1aa33fd26cbaf7b6853dcdb19c
ESSARRAJ <essarraj.fouad@gmail.com>
2024-03-21 07:56:31 +0000

    save

--- issues/update-issues-from-backlog.ps1
+++ issues/update-issues-from-backlog.ps1
@@ -1,6 +1,6 @@
 ﻿# Create or updat backlog to issues
 
-# TODO : Traiter suelement les fichiers siter dans le fichier backlog.json
+# TODO : Traiter suelement les fichiers citer dans le fichier backlog.json
 
 . "./scripts/core/core.ps1"
 . "./scripts/core/issue.core.ps1"
@@ -66,22 +66,40 @@ function edit_issue($Issue_obj,$label){
 }
 
 
+function add_or_update_issue( $file_fullname,$file_name, $label){
+
+  confirm_to_continue "add_or_update_issue : file_fullname =$file_fullname
+  `n - file_name =$file_name 
+  `n - label =$label"
+
+ 
+  $add_or_update_issues_chaned_files = $false
+
+  $item_full_path = Split-Path  -Path $file_fullname
+  # CreateIssue_obj that represente backlog_itm_file
+  $Issue_obj = get_issue_object $file_name  $file_fullname
+  if($Issue_obj.number -eq 0){ create_issue $Issue_obj $label
+  }else{ edit_issue $Issue_obj $label }
+
+  # Change backlog_item_file name
+  $change_backlog_item_file_name_retuen_value = change_backlog_item_file_name $file_fullname $file_name $Issue_obj
+
+   # if not yet true
+  if($change_backlog_item_file_name_retuen_value) {
+    $add_or_update_issues_chaned_files = $true  
+  }
+
+  return  $add_or_update_issues_chaned_files
+
+}
+
+
 $add_or_update_issues_iteration = 0
 
-# Ajouter ou créer une issue
-function add_or_update_issues($directory, $label){
+# Ajouter ou créer une issue dans le dossier $directory avec label : $label
+function add_or_update_issues_in_directory($directory, $label){
 
  confirm_to_continue "----`n - Update or Create issues for : $label `n - ----"
-
-
-  # # Update files 
-  # $updated_files = git log -1 --name-only --oneline
-  # for($i = 0; $i -lt $updated_files.lenght; $i++ ){
-  #   $updated_files[$i] = $update_file.Split("/")[$update_file.Split("/").lenght -1]
-  # }
-  # foreach($update_file in $updated_files){
-  #   Write-Host "update_file =$update_file"
-  # }
 
   $backlog_items=  Get-ChildItem $directory -Filter *.md  
   $add_or_update_issues_chaned_files = $false
@@ -90,6 +108,8 @@ function add_or_update_issues($directory, $label){
     $file_fullname = $backlog_item.FullName
     $file_name = $backlog_item.Name
 
+    $add_or_update_issues_chaned_files = add_or_update_issue $file_fullname $file_name $label 
+
     #  confirm_to_continue "if $file_name in  "
     # if($updated_files.Contains($file_name) ){
     #   confirm_to_continue "----`n - Update or Create issues for : $label `n - ----"
@@ -97,23 +117,7 @@ function add_or_update_issues($directory, $label){
     #   confirm_to_continue "if $file_name not in $updated_files  "
     #   continue
     # }
-    
-
-
-    $item_full_path = Split-Path  -Path $file_fullname
-    # CreateIssue_obj that represente backlog_itm_file
-    $Issue_obj = get_issue_object $file_name  $file_fullname
-    if($Issue_obj.number -eq 0){ create_issue $Issue_obj $label
-    }else{ edit_issue $Issue_obj $label }
-
-    # Change backlog_item_file name
-    $change_backlog_item_file_name_retuen_value = change_backlog_item_file_name $file_fullname $file_name $Issue_obj
-
-     # if not yet true
-    if($change_backlog_item_file_name_retuen_value) {
-      $add_or_update_issues_chaned_files = $true  
-    }
-
+  
     # En cas de test traiter un seul fichier par dossier
     if($test ) { break }
 
@@ -136,32 +140,66 @@ create_branch_to_do_pull_request $branche_name
 # des fichiers backlog item 
 $chaned_files = $false
 
-# Dans le dossier backlog, il existe plusieurs dossiers qui représente les labels 
-$backlog_directories=  Get-ChildItem "$depot_path/backlog"  -Directory
 
-foreach($backlog_directory in $backlog_directories) {
+# Les fichiers changés 
+$updated_files = git log -1 --name-only --oneline
+debug "Les fichiers changés :"
+debug $updated_files
 
-    # Ne pas traiter les dossier qui commance par "_"
-    if($backlog_directory.Name -like "_*") {continue}
+for($i = 0; $i -lt $updated_files.Count; $i++ ){
 
-    $label = $backlog_directory.Name
-    $directory = $backlog_directory.FullName 
-    $return_value = add_or_update_issues $directory $label
-    
-    # if not yet true
-    # Dans Powershell, les fonctions ne retourn pas une valeur mais le résultat de console
-    if(-not($chaned_files)) {
-      $chaned_files = $return_value[$return_value.lenght - 1]
-    }
-    
-    
+  # traiter seulement les fichiers dans le dossiere /backlog
+  if(-not($updated_files[$i] -like "backlog/**")){continue}
+
+  # Calculer les variables : $file_fullname,$file_name,$issue_label
+  $issue_label =$null
+  if($updated_files.Count -gt 1 ){
+    $issue_label = $updated_files[$i].Split("/")[$updated_files[$i].Split("/").Count -2]
+  }
+  $file_fullname =  $updated_files[$i]
+  $updated_files[$i] = $updated_files[$i].Split("/")[$updated_files[$i].Split("/").Count -1]
+  $file_name = $updated_files[$i]
+ 
+
+  $return_value = add_or_update_issue $file_fullname $file_name $issue_label
+
+  # if not yet true
+  # Dans Powershell, les fonctions ne retourn pas une valeur mais le résultat de console
+  if(-not($chaned_files)) {
+    $chaned_files = $return_value[$return_value.lenght - 1]
+  }
+
 }
 
-$add_issue_from_github_return_values = add_issue_from_github $depot_path
-if(-not($chaned_files)) {
-  $chaned_files = $add_issue_from_github_return_values[$add_issue_from_github_return_values.lenght - 1]
-}
+# exit 0
 
+
+# # Dans le dossier backlog, il existe plusieurs dossiers qui représente les labels 
+# $backlog_directories=  Get-ChildItem "$depot_path/backlog"  -Directory
+
+# foreach($backlog_directory in $backlog_directories) {
+
+#     # Ne pas traiter les dossier qui commance par "_"
+#     if($backlog_directory.Name -like "_*") {continue}
+
+#     $label = $backlog_directory.Name
+#     $directory = $backlog_directory.FullName 
+#     $return_value = add_or_update_issues_in_directory $directory $label
+    
+#     # if not yet true
+#     # Dans Powershell, les fonctions ne retourn pas une valeur mais le résultat de console
+#     if(-not($chaned_files)) {
+#       $chaned_files = $return_value[$return_value.lenght - 1]
+#     }
+    
+    
+# }
+
+# Ajouter les issues qui n'existe pas dans le backlog
+# $add_issue_from_github_return_values = add_issue_from_github $depot_path
+# if(-not($chaned_files)) {
+#   $chaned_files = $add_issue_from_github_return_values[$add_issue_from_github_return_values.lenght - 1]
+# }
 
 # Envoie de pullrequest si le programme à modifier les nom des fichiers backlog item
 save_and_send_pullrequest_if_files_changes $branche_name $chaned_files 
