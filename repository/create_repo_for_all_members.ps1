602777bc291d48e7ea2443f7bec15287428a1737
= <=>
2024-04-05 08:48:45 +0000

    create report for all members

---
+++ repository/create_repo_for_all_members.ps1
@@ -0,0 +1,22 @@
+﻿# Create repositories for all members
+
+$members = "Betroji-Jalil", 
+"Assaid-Amina",
+"Faiz-Safaa",
+"Achaou-Hamid",
+"Boukhar-Soufiane",
+"Daifane-Yasmine",
+"Lamchatab-Amine",
+"Bouik-Hussein",
+"Zaani-Hamza",
+"Grain-Reda",
+"Lharrak-Adnan",
+"Sarsri-Imrane",
+"Ben-nasar-adnan"
+
+foreach ($member in $members) {
+
+    gh repo create "solicoders/$member-autoformation-android" --public
+}
+
+
